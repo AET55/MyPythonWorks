aa3772742d4f6b510e5c369cb3c127aae062c245
AET55 <alperentul55@gmail.com>
2024-12-06 23:46:48 +0300

    test

--- variables-demo.py
+++ variables-demo.py
@@ -7,4 +7,4 @@ musteriDogumYili = 2003
 musteriAdresBilgisi = ' Sabanoglu Mahallesi Koroglu Sokak No 11 Kat 2 '
 musteriYasi = 21    
 print(musteriAdresBilgisi)
-print(110 + 1100.5 + 356.95)  
+print(110 + 1100.5 + 356.95)   
