eafb56a9d05363e07e12b9bd043618c11901af54
AET55 <alperentul55@gmail.com>
2024-12-06 23:42:25 +0300

    variablesdemo added

---
+++ variables-demo.py
@@ -0,0 +1,10 @@
+musteriAdi = 'Omar'
+musteriSoyadi = 'Kisa'
+musteriAdSoyad =  musteriAdi + ' ' + musteriSoyadi 
+musteriCinsiyet = ' Bayan '
+musteriTcKimlik = 15975345600
+musteriDogumYili = 2003
+musteriAdresBilgisi = ' Sabanoglu Mahallesi Koroglu Sokak No 11 Kat 2 '
+musteriYasi = 21    
+print(musteriAdresBilgisi)
+print(110 + 1100.5 + 356.95)  
