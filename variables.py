e271f4d226a3426025302cd621370348d291a4b8
AET55 <alperentul55@gmail.com>
2024-12-13 01:33:51 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+"""
+maasRecep = 10000
+maasTayyip = 12000
+vergi = 0.34
+print(maasRecep - (maasRecep * vergi))
+print(maasTayyip - (maasTayyip * vergi))
+number1 = 50
+number1 += 30
+print(number1)
+x, y, name, isTeacher = (33, 42.4, "Hasan", False)
+print(x, y, name, isTeacher)
+x = "25"
+y = "27"
+print(x+y)
+# now we wil learn how to input values
+x = input("1.sayi = ")
+y = input("2.sayi = ")
+print(type(x))
+print(type(y))
+print(x + y)
+print(int(x)+int(y))
+"""
+# type conversion - float to int - int to float
+x = 3
+y = 4.7
+x = float(x)
+y = int(y)
+print(x, y)
+isOnline = True
+isOffline = False
+isOnline = int(isOnline)
+isOffline = float(isOffline)
+print(isOnline, isOffline) 
