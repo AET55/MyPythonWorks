97aa3eb3e4f10b3ca12aa2f25f2e81ddcb9ffb61
AET55 <alperentul55@gmail.com>
2024-12-17 01:17:38 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+name = "Alperen"
+surName = " Tul"
+age = 27
+print("My name is "+name+ " " +surName+" and I am "+str(age)+" years old.")
+print("My name is "+name+ " " +surName+" and \nI am "+str(age)+" years old.") 
+greeting = "My name is "+name+ " " +surName+" and I am "+str(age)+" years old."
+print(greeting[8])
+print(len(greeting))
+print(greeting[-1])
+print(greeting[-4])
+print(greeting[43])
+print(greeting[11:18])
+print(greeting[:11])
+print(greeting[11:])
+print(greeting[1:46:3])
